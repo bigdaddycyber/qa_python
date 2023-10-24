cc16a688e76eec7c3e7a2816f44d89066c682c6f
Sergio <cyberprotectrus@gmail.com>
2023-10-24 13:12:40 +0300

    finish test

---
+++ conftest.py
@@ -0,0 +1,18 @@
+import pytest
+
+from main import BooksCollector
+
+@pytest.fixture
+def collector():
+    collector = BooksCollector()
+    collector.add_new_book('Гордость и предубеждение и зомби')
+    collector.set_book_genre('Гордость и предубеждение и зомби', 'Детективы')
+    collector.add_new_book('Что делать, если ваш кот хочет вас убить')
+    collector.set_book_genre('Что делать, если ваш кот хочет вас убить', 'Фантастика')
+    collector.add_new_book('Крошка - коврошка')
+    collector.set_book_genre('Крошка - коврошка', 'Мультфильмы')
+    return collector 
+    
+    
+    
+    
